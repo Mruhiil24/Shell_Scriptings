05c4db670cc2167f6121c0a5fbdf1de77e22f247
Moneka Ruhiil <84884354+Mruhiil24@users.noreply.github.com>
2025-05-18 21:12:01 -0700

    Create Backup.sh

---
+++ Backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+<< note
+This script takes backup of any destination path given as an argument
+Usage: ./backup.sh /path/to/folder
+note
+
+# Step 1: Get the current timestamp
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
+
+# Step 2: Set the backup file name and location
+backup_dir="/home/ubuntu/backups/${timestamp}_backup.zip"
+
+# Step 3: Create a zip backup of the folder passed as $1 (first argument)
+zip -r $backup_dir $1
+
+# Step 4: Confirm completion
+echo "Backup Complete"
